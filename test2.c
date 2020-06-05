4fcf6fb42766b6610976a5477681977fabd7bade
t190051 <63433796+t190051@users.noreply.github.com>
2020-06-05 10:17:23 +0900

    Update test2.c

--- test2.c
+++ test2.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int even(int a){
+    return a/2;
+}
+int kis(int a){
+    return 3*a+1;
+}
+
+int main(void){
+    int a=245845,n;
+    while(1){
+        if(a%2==1)a=kis(a);
+        if(a%2==0)a=even(a);
+        printf("%d\n",a);
+        if(a==1)break;
+    }
+}
