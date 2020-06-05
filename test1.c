015aa766878ddc11cc68eb69fa42e25ab605f11f
t190051 <63433796+t190051@users.noreply.github.com>
2020-06-05 09:54:26 +0900

    Update test1.c

--- test1.c
+++ test1.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main(void){
+    int a=1,i,n=1;
+    long int f=1;
+    while(1){
+        for(i=1;i<n;i++){
+            f=f*n;
+        }
+        if(f<2147483647)a=f;
+        else break;
+        printf("%d\n",a);
+        n++;
+        f=n;
+    }
+}
